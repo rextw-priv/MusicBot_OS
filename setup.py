31f347869e4cc96927eb6a40540a4bbab15ca399
RexTW <rexx0520@gmail.com>
2017-08-04 19:13:43 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gnehsMusicBot',
+      version='1.0',
+      description='Music!',
+      author='Rex',
+      author_email='rexx0520@gmail.com',
+      url='http://t.me/rextw',
+#      install_requires=['Django>=1.3'],
+      install_requires=['aiotg>=0.7.3'],
+     )
