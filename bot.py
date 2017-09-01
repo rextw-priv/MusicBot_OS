e54e526cec37557c206a5327351bfe8776650b11
RexTW <rexx0520@gmail.com>
2017-09-01 22:50:32 +0800

    bug fixed

--- bot.py
+++ bot.py
@@ -179,7 +179,7 @@ def music(chat, match):
 def whoami(chat, match):
     return chat.reply(chat.sender["id"])
 
-@bot.command(r'\((\d+)/\d+\) @%s "(.+)"' % texts['nextPage'])
+@bot.command(r'\((\d+)/\d+\) %s "(.+)"' % texts['nextPage'])
 def more(chat, match):
     page = int(match.group(1))
     return search_tracks(chat, match.group(2), page)
