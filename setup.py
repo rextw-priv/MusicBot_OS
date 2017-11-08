3d59ff2c09032af821b08be272810dc67e8e5ba2
RexTW <rexx0520@gmail.com>
2017-11-08 21:37:52 +0800

    ㄍ

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='gnehsMusicBot',
       author_email='rexx0520@gmail.com',
       url='http://t.me/rextw',
 #      install_requires=['Django>=1.3'],
-      install_requires=['aiotg>=0.7.3'],
+      install_requires=['aiotg>=0.7.3', 'motor>=0.5'],
      )
